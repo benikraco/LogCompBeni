23c4b9b3de9f9d4f13b66a0f84389702d96a8dad
benikraco <benik@al.insper.edu.br>
2023-03-05 16:03:54 -0300

    de prima?

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# LogComp
+# Repositório de Lógica da Computação
+
+### Status dos testes
+
+![git status](http://3.129.230.99/svg/benikraco/LogCompBeni/)
+
+#### Diagrama sintático
+
+![1677527145466](image/README/1677527145466.png)
+
+#### EBNF
+
+```python
+EXPRESSION = TERM, { ("+" | "-"), TERM };
+TERM = NUMBER, { ("*" | "/"), NUMBER };
+```
--- main.py
+++ main.py
@@ -34,6 +34,18 @@ class Tokenizer:
                 self.position += 1
                 return self.next
             
+            # check for multiplication
+            elif self.source[self.position] == "*":
+                self.next = Token("MULT", self.source[self.position])
+                self.position += 1
+                return self.next
+
+            # check for division
+            elif self.source[self.position] == "/":
+                self.next = Token("DIV", self.source[self.position])
+                self.position += 1
+                return self.next
+            
             # check for space
             elif self.source[self.position].isdigit():
                 int = ""
@@ -42,6 +54,8 @@ class Tokenizer:
                     self.position += 1
                 self.next = Token("INT", int)
                 return self.next
+            
+            # check for error
             else:
                 self.next = Token("ERROR", self.source[self.position])
                 self.position += 1
@@ -55,9 +69,12 @@ class Tokenizer:
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
-    
+
     @staticmethod
-    def parseExpression(tokens):
+    # parse multiplication and division
+    def ParseTerm(tokens):
+
+        # skip spaces
         tokens.selectNext()
         result = 0
 
@@ -65,54 +82,69 @@ class Parser:
         while tokens.next.type == "SPACE":
             tokens.selectNext()
 
-        
         # check for error
         if tokens.next.type == "ERROR":
             sys.stderr.write('[ERRO]\n')
             sys.exit()
-        
-        # check for int
+
+        # check for integer
         if tokens.next.type == "INT":
             result = int(tokens.next.value)
             tokens.selectNext()
-
-            if tokens.next.type == "INT":
-                sys.stderr.write('[ERRO]\n')
-                sys.exit()
-
-            # skip spaces
-            while tokens.next.type == "SPACE":
-                tokens.selectNext()
-                
-            while tokens.next.type == "PLUS" or tokens.next.type == "MINUS":
-                if tokens.next.type == "PLUS":
-                    tokens.selectNext()
-                    while tokens.next.type == "SPACE":
-                        tokens.selectNext()
-                    if tokens.next.type == "INT":
-                        result += int(tokens.next.value)
-                        tokens.selectNext()
-                    else:
-                        sys.stderr.write('[ERRO]\n')
-                        sys.exit()
-
-                elif tokens.next.type == "MINUS":
-                    tokens.selectNext()
-                    while tokens.next.type == "SPACE":
-                        tokens.selectNext()
-                    if tokens.next.type == "INT":
-                        result -= int(tokens.next.value)
-                        tokens.selectNext()
-                    else:
-                        sys.stderr.write('[ERRO]\n')
-                        sys.exit()
-
-            if tokens.next.type == "EOF":
-                return result
             
+            # check for multiplication or division
+            while tokens.next.type == "MULT" or tokens.next.type == "DIV":
+
+                # check for multiplication
+                if tokens.next.type == "MULT":
+                    tokens.selectNext()
+                    while tokens.next.type == "SPACE":
+                        tokens.selectNext()
+                    if tokens.next.type == "INT":
+                        result *= int(tokens.next.value)
+                        tokens.selectNext()
+                    else:
+                        sys.stderr.write('[ERRO]\n')
+                        sys.exit()
+                
+                # check for division
+                elif tokens.next.type == "DIV":
+                    tokens.selectNext()
+                    while tokens.next.type == "SPACE":
+                        tokens.selectNext()
+                    if tokens.next.type == "INT":
+                        result /= int(tokens.next.value)
+                        tokens.selectNext()
+                    else:
+                        sys.stderr.write('[ERRO]\n')
+                        sys.exit()
+
+                if tokens.next.type == "EOF":
+                    return result
+                    
+                else:
+                    sys.stderr.write('[ERRO]\n')
+                    sys.exit()
+
+            return result
+        
         else:
             sys.stderr.write('[ERRO]\n')
             sys.exit()
+    
+    @staticmethod
+    def parseExpression(tokens):
+        result = Parser.ParseTerm(tokens)
+
+        while tokens.next.type == "PLUS" or tokens.next.type == "MINUS":
+            if tokens.next.type == "PLUS":
+                result += Parser.ParseTerm(tokens)
+            elif tokens.next.type == "MINUS":
+                result -= Parser.ParseTerm(tokens)
+        
+        tokens.selectNext()
+        if tokens.next.type == "EOF":
+            return result
     
     @staticmethod
     def run(code):
@@ -121,14 +153,33 @@ class Parser:
 
         if parsed is not None:
             return parsed
+        
+class PrePro:
+    @staticmethod
+
+    # filter the input excluding comments
+    def filter(text):
+        comment = False
+        filtered = ""
+
+        for i in range(len(text)):
+            if text[i] == "/" and text[i + 1] == "/":
+                if i>2:
+                    filtered = filtered[:i-2]
+                else:
+                    filtered = filtered[:i-1]
+                comment = True
+                break
+            if not comment:
+                filtered += text[i]
+        
+        return filtered
+
 
 # define the main function
 def main():
-    try:
-        result = Parser.run(input_expression)
-        print(result)
-    except Exception as e:
-        print("Error:", e)
+    result = Parser.run(PrePro.filter(input_expression))
+    print(result)
 
 if __name__ == "__main__":
     main()
